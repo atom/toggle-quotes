c17f3c2f200be51bf0138d6176a7965292e1d1a7
Machiste Quintana <mnquintana@users.noreply.github.com>
2015-07-17 19:04:48 -0400

    WIP: Convert specs to ES6

---
+++ spec/toggle-quotes-es6-spec.js
@@ -0,0 +1,54 @@
+'use babel';
+
+import {toggleQuotes} from '../lib/toggle-quotes-es6';
+
+describe("ToggleQuotes", () => {
+  beforeEach(() => {
+    atom.config.set('toggle-quotes.quoteCharacters', '\'"');
+  });
+
+  describe("toggleQuotes(editor) js", () => {
+    let editor = null;
+
+    beforeEach(() => {
+      waitsForPromise(() => {
+        return atom.packages.activatePackage('language-javascript');
+      });
+
+      waitsForPromise(() => {
+        return atom.packages.activatePackage('language-json');
+      });
+
+      waitsForPromise(() => {
+        return atom.workspace.open();
+      });
+
+      runs(() => {
+        editor = atom.workspace.getActiveTextEditor();
+        editor.setText(
+          `console.log("Hello World");
+          console.log('Hello World');
+          console.log("Hello 'World'");
+          console.log('Hello "World"');
+          console.log('');`
+        );
+        editor.setGrammar(atom.grammars.selectGrammar('test.js'));
+      });
+    });
+
+    describe("when the cursor is not inside a quoted string", () => {
+      it("does nothing", () => {
+        expect(() => toggleQuotes(editor)).not.toThrow();
+      });
+    });
+
+    describe("when the cursor is inside an empty single quoted string", () => {
+      it("switches the quotes to double", () => {
+        editor.setCursorBufferPosition([4, 13]);
+        toggleQuotes(editor);
+        expect(editor.lineTextForBufferRow(4)).toBe('console.log("");');
+        expect(editor.getCursorBufferPosition()).toEqual([4, 13]);
+      });
+    });
+  });
+});
